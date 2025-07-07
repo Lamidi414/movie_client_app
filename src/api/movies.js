fb02f1b7e65bd59fe06d7c8ab3082750c329598b
Lamidi414 <abdulhamidsalisu2005@gmail.com>
2025-07-07 20:47:59 +0100

    API call in realtime

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_API_BASE_URL
+    : 'http://localhost:5000/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
